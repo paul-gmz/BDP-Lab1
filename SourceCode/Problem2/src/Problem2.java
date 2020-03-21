5ad7097dc094f0f0d9b440b65fd772c220451e86
Paul Gomes <gomesap29@gmail.com>
2020-03-21 01:31:26 -0700

    progress on problem 2

--- SourceCode/Problem2/src/Problem2.java
+++ SourceCode/Problem2/src/Problem2.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -13,37 +14,43 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class Problem1 {
+public class Problem2 {
 
     public static class TokenizerMapper
-            extends Mapper<LongWritable, Text, Text, IntWritable> {
-
-        private final static IntWritable one = new IntWritable(1);
-        private Text category = new Text();
+            extends Mapper<LongWritable, Text, Text, FloatWritable> {
+    	
+        private Text videoId = new Text();
+        private FloatWritable ratings = new FloatWritable();
 
         public void map(LongWritable key, Text value, Context context
         ) throws IOException, InterruptedException {
         	String singleLine = value.toString();
         	String col[] = singleLine.split("\t");
-        	if(col.length > 4){
-        		category.set(col[3]);
-                context.write(category, one);
+        	if(col.length > 7){
+        		videoId.set(col[0]);
+                if(col[6].matches("\\d+.+")){
+                	float rating = Float.parseFloat(col[6]);
+                	ratings.set(rating);
+                }
+                context.write(videoId, ratings);
         	}
         }
     }
 
-    public static class IntSumReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
+    public static class AvgRatingsReducer
+            extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+        private FloatWritable result = new FloatWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
+        public void reduce(Text key, Iterable<FloatWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
             int sum = 0;
-            for (IntWritable val : values) {
+            int count = 0;
+            for (FloatWritable val : values) {
+            	count += 1;
                 sum += val.get();
             }
-            result.set(sum);
+            result.set((sum/count));
             context.write(key, result);
         }
     }
@@ -51,10 +58,10 @@ public class Problem1 {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Problem 1");
-        job.setJarByClass(Problem1.class);
+        job.setJarByClass(Problem2.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setCombinerClass(AvgRatingsReducer.class);
+        job.setReducerClass(AvgRatingsReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
